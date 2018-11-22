3a2388bcccd8631c613065a4a7a7015db334ee56
Tom Stephens <thomas.stephens@gmail.com>
2018-11-22 11:37:54 -0700

    Initial loop for filling SSC and finishing ships.
    Need to add in initial conditions, statistics, and clean up output formatting

--- src/SatharConstruction.py
+++ src/SatharConstruction.py
@@ -4,6 +4,11 @@ Created on Nov 22, 2018
 
 @author: Tom Stephens
 '''
+# For this simulation we will be controlling the types of ships
+# created in each of the individual SCCs as well as the start date
+# for ship manufacture at each SCC.  We'll also be outputting the
+# total number of each type of ship at regular intervals
+
 
 import random
 import math
@@ -11,7 +16,39 @@ import sys
 
 from StarshipConstructionCenter import StarshipConstructionCenter
 from Starship import Starship
+from enum import Enum
 
+class ShipType(Enum):
+    F = 1
+    C = 4
+    FF = 5
+    DD = 6
+    LC = 14
+    AC = 16
+    HC = 18
+
+SCCData = {
+          "SCC01" : { "HP":46, "start":868, "types":[ShipType.LC,ShipType.DD] },
+          "SCC02" : { "HP":46, "start":221, "types":[ShipType.AC,ShipType.DD,ShipType.F], "buildFighters": True },
+          "SCC03" : { "HP":47, "start":155, "types":[ShipType.HC,ShipType.DD,ShipType.FF] },
+          "SCC04" : { "HP":50, "start":196, "types":[ShipType.AC,ShipType.LC,ShipType.DD,ShipType.F], "buildFighters": True  },
+          "SCC05" : { "HP":50, "start":142, "types":[ShipType.LC,ShipType.DD,ShipType.C] },
+          "SCC06" : { "HP":200, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC07" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC08" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC09" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC10" : { "HP":60, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.F] }
+          }
+
+shipCount = {
+    ShipType.F: 0,
+    ShipType.C: 0,
+    ShipType.FF: 0,
+    ShipType.DD: 0,
+    ShipType.LC: 0,
+    ShipType.AC: 0,
+    ShipType.HC: 0,
+    }
 
 def createSSCList():
     '''
@@ -23,39 +60,80 @@ def createSSCList():
     '''
     centers = []
     i=0
-    #list is name, max hull sizes  All will be type 5
-    sccList = {
-               "SCC01" : 46,
-               "SCC02" : 46,
-               "SCC03" : 47,
-               "SCC04" : 50,
-               "SCC05" : 50,
-               "SCC06" : 200,
-               "SCC07" : 75,
-               "SCC08" : 75,
-               "SCC09" : 75,
-               "SCC10" : 60,
-               }
-    for key in sccList:
+    for key in SCCData:
         centers.append( StarshipConstructionCenter( 5, key ) )
-        centers[i].maxTotalHullSizes = sccList[key]
+        centers[i].maxTotalHullSizes = SCCData[key]["HP"]
+        centers[i].nextShipSize = SCCData[key]["HP"]
         i += 1
     return centers
 
+def checkBuildFighters(scc,type):
+    if (SCCData[scc]["buildFighters"] == True):
+        SCCData[scc]["buildFighters"] = False
+        return ShipType.F.value
+    else:
+        SCCData[scc]["buildFighters"] = True
+        return type.value
+
+
+def nextShip(scc,space):
+    '''Returns the hull size of the next ship to start based on the SSC's
+    production schedule '''
+    size = 0
+    if ("SSC02" == scc or "SSC04" == scc):
+        for ship in SCCData[scc]["types"]:
+            if (space >= ship.value):
+                if (ShipType.DD):
+                    return checkBuildFighters(scc,ShipType.DD)
+                return ship.value
+    else:
+        for ship in SCCData[scc]["types"]:
+            if (space >= ship.value):
+                return ship.value
+    return size
 
 
 if __name__ == '__main__':
     print ("#Running Sathar Starship Construction Simulation.")
     sccs = createSSCList()
-    ships = []
-    newShips = []
-    repairShips = []
-    shipDist = {}
-    day = 1
-    daysToSimulate = 40000
+
+
+    day = 0
+    daysToSimulate = 800
 
     for scc in sccs:
         print (scc.getName(), scc.getMaxTotalHullSizes())
+
+    ships = []
+    while (day < daysToSimulate):
+        year = 59 + day//400
+        date = 1 + day%400
+#        print(year,".",date)
+        for scc in sccs:
+            if (day >= SCCData[scc.getName()]["start"]): #only update the SCC if past its "start date"
+                # first get all the ships that were completed (if any)
+                finishedShips = scc.update(day)
+                if (len(finishedShips) > 0):
+                    for ship in finishedShips:
+                        for type in ShipType:
+                            if (type.value == ship.getHullSize()):
+                                print(year,".",date,"-",scc.getName(),"produced a",type.name)
+                                ships.append(ship)
+                                shipCount[type] += 1
+                # next add in a new ship if there is room
+                ahs = scc.getAvailableSpace()
+                while (ahs > 0):
+                    size = nextShip(scc.getName(),ahs)
+                    if (size > 0):
+                        #create new ship of the returned size and add it the scc's queue
+                        newShip = Starship( size, 'military', 100 )
+                        scc.addToQueue(newShip)
+                        ahs -= size
+                    else:
+                        break
+        day += 1
+
+
 
 
 
