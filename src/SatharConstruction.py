094b4cca2f622262bb83130b217ba1611d888a7e
Tom Stephens <thomas.stephens@gmail.com>
2018-11-27 11:39:14 -0700

    Added iniital SCC production
    Also fixed a bug in the add figthers routine

--- src/SatharConstruction.py
+++ src/SatharConstruction.py
@@ -28,16 +28,16 @@ class ShipType(Enum):
     HC = 18
 
 SCCData = {
-          "SCC01" : { "HP":46, "start":868, "types":[ShipType.LC,ShipType.DD] },
-          "SCC02" : { "HP":46, "start":221, "types":[ShipType.AC,ShipType.DD,ShipType.F], "buildFighters": True },
-          "SCC03" : { "HP":47, "start":155, "types":[ShipType.HC,ShipType.DD,ShipType.FF] },
-          "SCC04" : { "HP":50, "start":196, "types":[ShipType.AC,ShipType.LC,ShipType.DD,ShipType.F], "buildFighters": True  },
+          "SCC01" : { "HP":48, "start":868, "types":[ShipType.LC,ShipType.DD] },
+          "SCC02" : { "HP":46, "start":221, "types":[ShipType.AC,ShipType.DD,ShipType.F], "buildFighters": False },
+          "SCC03" : { "HP":59, "start":155, "types":[ShipType.HC,ShipType.DD,ShipType.FF] },
+          "SCC04" : { "HP":49, "start":196, "types":[ShipType.AC,ShipType.LC,ShipType.FF,ShipType.F], "buildFighters": False  },
           "SCC05" : { "HP":50, "start":142, "types":[ShipType.LC,ShipType.DD,ShipType.C] },
-          "SCC06" : { "HP":200, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
-          "SCC07" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
-          "SCC08" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
-          "SCC09" : { "HP":75, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
-          "SCC10" : { "HP":60, "start":5000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.F] }
+          "SCC06" : { "HP":200, "start":50000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC07" : { "HP":75, "start":50000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC08" : { "HP":75, "start":50000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC09" : { "HP":75, "start":50000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.C,ShipType.F] },
+          "SCC10" : { "HP":60, "start":50000, "types":[ShipType.HC,ShipType.LC,ShipType.AC,ShipType.DD,ShipType.FF,ShipType.F] }
           }
 
 shipCount = {
@@ -49,6 +49,81 @@ shipCount = {
     ShipType.AC: 0,
     ShipType.HC: 0,
     }
+
+def initializeSCCs(centers):
+    '''Set up the initial state of the various SCCs based on their
+    production schedule'''
+    y=1000 #we're not worrying about maintenance in this sim
+
+    #SCC 1
+    lc = Starship(14,'military',y)
+    lc.daysLeft = 140
+    centers[0].addToQueue(lc)
+    lc = Starship(14,'military',y)
+    lc.daysLeft = 280
+    centers[0].addToQueue(lc)
+    centers[0].addToQueue(Starship(14,'military',y))
+    #dd = Starship(6,'military',y)
+    #dd.daysLeft = 60
+    #centers[0].addToQueue(dd)
+    #dd = Starship(6,'military',y)
+    #dd.daysLeft = 120
+    #centers[0].addToQueue(dd)
+    centers[0].addToQueue(Starship(5,'military',y))
+
+    #SCC 2
+    ac = Starship(16,'military',y)
+    centers[1].addToQueue(ac)
+    dd = Starship(6,'military',y)
+    dd.daysLeft = 45
+    centers[1].addToQueue(dd)
+    dd = Starship(6,'military',y)
+    dd.daysLeft = 90
+    centers[1].addToQueue(dd)
+    dd = Starship(6,'military',y)
+    dd.daysLeft = 135
+    centers[1].addToQueue(dd)
+    dd = Starship(6,'military',y)
+    centers[1].addToQueue(dd)
+    centers[1].addToQueue(Starship(1,'military',y))
+    centers[1].addToQueue(Starship(1,'military',y))
+    centers[1].addToQueue(Starship(1,'military',y))
+    centers[1].addToQueue(Starship(1,'military',y))
+    centers[1].addToQueue(Starship(1,'military',y))
+    centers[1].addToQueue(Starship(1,'military',y))
+
+    #SCC 3
+    hc = Starship(18,'military',y)
+    hc.daysLeft = 180
+    centers[2].addToQueue(hc)
+    hc = Starship(18,'military',y)
+    hc.daysLeft = 360
+    centers[2].addToQueue(hc)
+    centers[2].addToQueue(Starship(18,'military',y))
+#    centers[2].addToQueue(Starship(6,'military',y))
+    centers[2].addToQueue(Starship(5,'military',y))
+
+    #SCC 4
+    centers[3].addToQueue(Starship(16,'military',y))
+    lc = Starship(14,'military',y)
+    lc.daysLeft = 210
+    centers[3].addToQueue(lc)
+    centers[3].addToQueue(Starship(14,'military',y))
+    centers[3].addToQueue(Starship(1,'military',y))
+    centers[3].addToQueue(Starship(1,'military',y))
+    centers[3].addToQueue(Starship(1,'military',y))
+    centers[3].addToQueue(Starship(1,'military',y))
+    centers[3].addToQueue(Starship(1,'military',y))
+#    centers[3].addToQueue(Starship(1,'military',y))
+
+    #SCC 5
+    lc = Starship(14,'military',y)
+    lc.daysLeft = 210
+    centers[4].addToQueue(lc)
+    centers[4].addToQueue(Starship(14,'military',y))
+    centers[4].addToQueue(Starship(18,'military',y))
+    centers[4].addToQueue(Starship(4,'military',y))
+
 
 def createSSCList():
     '''
@@ -65,6 +140,7 @@ def createSSCList():
         centers[i].maxTotalHullSizes = SCCData[key]["HP"]
         centers[i].nextShipSize = SCCData[key]["HP"]
         i += 1
+    initializeSCCs(centers)
     return centers
 
 def checkBuildFighters(scc,type):
@@ -80,11 +156,17 @@ def nextShip(scc,space):
     '''Returns the hull size of the next ship to start based on the SSC's
     production schedule '''
     size = 0
-    if ("SSC02" == scc or "SSC04" == scc):
+    if ("SCC02" == scc):
         for ship in SCCData[scc]["types"]:
             if (space >= ship.value):
-                if (ShipType.DD):
+                if (ShipType.DD == ship):
                     return checkBuildFighters(scc,ShipType.DD)
+                return ship.value
+    elif ("SCC04" == scc):
+        for ship in SCCData[scc]["types"]:
+            if (space >= ship.value):
+                if (ShipType.FF == ship):
+                    return checkBuildFighters(scc,ShipType.FF)
                 return ship.value
     else:
         for ship in SCCData[scc]["types"]:
@@ -99,7 +181,7 @@ if __name__ == '__main__':
 
 
     day = 0
-    daysToSimulate = 800
+    daysToSimulate = 3*400
 
     #for scc in sccs:
     #    print (scc.getName(), scc.getMaxTotalHullSizes())
@@ -116,8 +198,8 @@ if __name__ == '__main__':
                 if (len(finishedShips) > 0):
                     for ship in finishedShips:
                         for type in ShipType:
-                            if (type.value == ship.getHullSize()):
-                                #print(year,".",date,"-",scc.getName(),"produced a",type.name)
+                            if (day>=400 and type.value == ship.getHullSize()):
+                                print(str(year)+"."+str(date),"-",scc.getName(),"produced a",type.name)
                                 ships.append(ship)
                                 shipCount[type] += 1
                 # next add in a new ship if there is room
